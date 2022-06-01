e4cf97486165e846a9d7017fd148cb37957cb4bc
zRafael <rafael.silva@bandtec.com.br>
2022-06-01 18:08:56 -0300

    Adicionando model,service e outras funcionalidades

---
+++ blogPessoalFront/src/app/model/User.ts
@@ -0,0 +1,14 @@
+import { Postagem } from "./Postagem";
+
+export class User {
+
+    public id: number;
+    public nome: string;
+    public usuario: string;
+    public senha: string;
+    public token: string;
+    public foto: string;
+    public tipo: string;  
+    public postagem: Postagem[];  
+
+}
