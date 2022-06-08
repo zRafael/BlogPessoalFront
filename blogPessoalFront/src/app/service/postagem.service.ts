cdfa8cffce4db6b7c24a1cfae48fc9979ad32cdc
zRafael <rafael.silva@bandtec.com.br>
2022-06-08 20:26:09 -0300

    Adicionando put e delete de postagem

--- blogPessoalFront/src/app/service/postagem.service.ts
+++ blogPessoalFront/src/app/service/postagem.service.ts
@@ -21,7 +21,20 @@ export class PostagemService {
     return this.http.get<Postagem[]>('https://projetoblogpessoal87.herokuapp.com/postagens', this.token)
   }
 
+  getByIdPostagem(id: number): Observable<Postagem> {
+    return this.http.get<Postagem>(`https://projetoblogpessoal87.herokuapp.com/postagens/${id}`, this.token)
+
+  }
+
   postPostagem(postagem: Postagem): Observable<Postagem> {
     return this.http.post<Postagem>('https://projetoblogpessoal87.herokuapp.com/postagens', postagem, this.token)
+  }
+
+  putPostagem(postagem: Postagem): Observable<Postagem> {
+    return this.http.put<Postagem>('https://projetoblogpessoal87.herokuapp.com/postagens', postagem, this.token)
+  }
+
+  deletePostagem(id: number) {
+    return this.http.delete(`https://projetoblogpessoal87.herokuapp.com/postagens/${id}`, this.token)
   }
 }
