43b6816f8ffae0aee299fc38175ea8d0302c5a16
zRafael <rafael.silva@bandtec.com.br>
2022-06-14 10:17:37 -0300

    Ajustes finais - Ordenação

--- blogPessoalFront/src/app/app.module.ts
+++ blogPessoalFront/src/app/app.module.ts
@@ -21,6 +21,7 @@ import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.compon
 import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
 import { UserEditComponent } from './edit/user-edit/user-edit.component';
 import { AlertasComponent } from './alertas/alertas.component';
+import { OrderModule } from 'ngx-order-pipe';
 
 
 
@@ -45,7 +46,8 @@ import { AlertasComponent } from './alertas/alertas.component';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    ModalModule.forRoot()
+    ModalModule.forRoot(), 
+    OrderModule
   ],
   providers: [{
     provide: LocationStrategy,
